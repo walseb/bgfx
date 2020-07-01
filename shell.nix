836815ec750b2394aedd2d9abdcdd72b546249c7
Sebastian Wålinder <s.walinder@gmail.com>
2020-07-01 07:31:38 +0200

    Now using cabal to build and nix for dependencies

--- shell.nix
+++ shell.nix
@@ -1,32 +1,13 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.pkg-config
+    pkgs.cabal-install
+    pkgs.ghc
 
-let
-  inherit (nixpkgs) pkgs;
-  f = { mkDerivation, base, bytestring, clock, distributive
-      , lens, linear, mesa, sdl2, stdenv, x11, libGL
-      }:
-      mkDerivation {
-        pname = "bgfx";
-        version = "0.1.0.0";
-        src = ./.;
-        isLibrary = true;
-        isExecutable = true;
-        libraryHaskellDepends = [ base ];
-        libraryPkgconfigDepends = [ mesa x11 libGL ];
-        executableHaskellDepends = [
-          base bytestring clock distributive lens linear sdl2
-        ];
-        homepage = "https://github.com/haskell-game/bgfx";
-        description = "Haskell bindings to bgfx";
-        license = stdenv.lib.licenses.bsd3;
-      };
-
-  haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
-
-  drv = haskellPackages.callPackage f {};
-
-in
-
-  if pkgs.lib.inNixShell then drv.env else drv
+    pkgs.SDL2
+    pkgs.libGL
+    pkgs.mesa
+    pkgs.x11
+  ];
+}
